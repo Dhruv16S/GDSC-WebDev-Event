e18367f41a2ddad0f3d69652c2abbdf26bfb3d86
Mayush <tmayush@gmail.com>
2023-03-30 19:59:17 +0530

    Refactored and optimized code HTML and CSS
    - Bootstrap is no longer required for the page
    - Removed most of the inline styles

--- script.js
+++ script.js
@@ -2,28 +2,30 @@ var countdownContainer = document.getElementById("countdown-container");
 const countdown = document.getElementById("countdown");
 const startButton = document.getElementById("start-button");
 
-document.addEventListener("contextmenu", function(event){
+document.addEventListener("contextmenu", function (event) {
+  event.preventDefault();
+  alert("Right click is not allowed!");
+});
+
+document.addEventListener("keydown", function (event) {
+  if (event.ctrlKey && (event.keyCode === 73 || event.keyCode === 85)) {
     event.preventDefault();
-    alert("Right click is not allowed!");
+    alert("Inspect mode is not allowed!");
+  }
 });
 
-document.addEventListener("keydown", function(event){
-    if (event.ctrlKey && (event.keyCode === 73 || event.keyCode === 85)) {
-        event.preventDefault();
-        alert("Inspect mode is not allowed!");
-    }
-});
-
-const countdownTime = 60*60;
+const countdownTime = 60 * 60;
 
 function formatTime(timeInSeconds) {
   const minutes = Math.floor(timeInSeconds / 60);
   const seconds = timeInSeconds % 60;
-  return `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+  return `${minutes.toString().padStart(2, "0")}:${seconds
+    .toString()
+    .padStart(2, "0")}`;
 }
 
-completionText = "<p style='font-weight: 600'>Thank you for participating!</p><img src='GDSC Logo.png' alt='Logo' height='55'><p style='font-weight: 600'>GDSC CBIT</p>";
-
+completionText =
+  "<p style='font-weight: 600'>Thank you for participating!</p><img src='GDSC Logo.png' alt='Logo' height='55'><p style='font-weight: 600'>GDSC CBIT</p>";
 
 function updateCountdown(endTime) {
   const remainingTime = Math.max(endTime - Date.now(), 0);
@@ -31,12 +33,12 @@ function updateCountdown(endTime) {
   if (remainingTime === 0) {
     clearInterval(timer);
     countdownContainer.innerHTML = completionText;
-    document.body.style.pointerEvents = "none"; 
-    return; 
+    document.body.style.pointerEvents = "none";
+    return;
   }
 }
 
-startButton.addEventListener("click", function() {
+startButton.addEventListener("click", function () {
   startButton.disabled = true;
   const endTime = Date.now() + countdownTime * 1000;
   updateCountdown(endTime);
