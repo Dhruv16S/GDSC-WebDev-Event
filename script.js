b1b21600d02686d1c1c08b363ec91a008e6f2e06
Dhruv Saxena <dhruv162002@gmail.com>
2023-03-30 14:08:44 +0530

    Fix countdown

--- script.js
+++ script.js
@@ -1,44 +1,48 @@
-var countdown = document.getElementById("countdown");
-var startButton = document.getElementById("start-button");
 var countdownContainer = document.getElementById("countdown-container");
-
-var count = 2700; // 45 minutes in seconds
-var timer = null;
-
+const countdown = document.getElementById("countdown");
+const startButton = document.getElementById("start-button");
 
 document.addEventListener("contextmenu", function(event){
     event.preventDefault();
     alert("Right click is not allowed!");
-  });
+});
 
 document.addEventListener("keydown", function(event){
     if (event.ctrlKey && (event.keyCode === 73 || event.keyCode === 85)) {
-      event.preventDefault();
-      alert("Inspect mode is not allowed!");
+        event.preventDefault();
+        alert("Inspect mode is not allowed!");
     }
-  });
+});
 
-  
+const countdownTime = 10;
+
 function formatTime(timeInSeconds) {
-  var minutes = Math.floor(timeInSeconds / 60);
-  var seconds = timeInSeconds % 60;
-  return minutes.toString().padStart(2, "0") + ":" + seconds.toString().padStart(2, "0");
+  const minutes = Math.floor(timeInSeconds / 60);
+  const seconds = timeInSeconds % 60;
+  return `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
 }
 
-completionText = "<p style = 'font-weight: 600'>Thank you for participating!</p><img src='GDSC Logo.png' alt='Logo' height='55'><p style = 'font-weight: 600'>GDSC CBIT</p>";
+completionText = "<p style='font-weight: 600'>Thank you for participating!</p><img src='GDSC Logo.png' alt='Logo' height='55'><p style='font-weight: 600'>GDSC CBIT</p>";
 
-function tick() {
-  countdown.innerHTML = formatTime(count);
-  if (count === 0) {
+
+function updateCountdown(endTime) {
+  const remainingTime = Math.max(endTime - Date.now(), 0);
+  countdown.innerHTML = formatTime(Math.ceil(remainingTime / 1000));
+  if (remainingTime === 0) {
     clearInterval(timer);
     countdownContainer.innerHTML = completionText;
+    document.body.style.pointerEvents = "none"; 
+    return; 
   }
-  count--;
 }
 
 startButton.addEventListener("click", function() {
   startButton.disabled = true;
-  timer = setInterval(tick, 1000);
+  const endTime = Date.now() + countdownTime * 1000;
+  updateCountdown(endTime);
+  timer = setInterval(() => {
+    updateCountdown(endTime);
+  }, 1000);
 });
 
-countdown.innerHTML = formatTime(count);
+countdown.innerHTML = formatTime(countdownTime);
